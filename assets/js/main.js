10400774552023e6d8c10df9fb2b5ba60e3601bc
marcobo <marcobrugo@mac.com>
2019-01-26 12:27:44 -0500

    pushng sliders

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,91 @@
 // console.log("on");
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+ 
+  // Or with jQuery
 $(document).ready(function () {
 
-
-
+    $('.slider').slider();
+    instance.start();
     
 
-     function calculateRoute(from, to) {
-          //use only  a single maps-instance
-          if (!window.mapObject) {
-              var myOptions = {
-                  zoom: 10,
-                  center: new google.maps.LatLng(40.84, 14.25),
-                  mapTypeId: google.maps.MapTypeId.ROADMAP
-              };
-              // Draw the map
-              mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-              // you also need only a single    DirectionsService-instance
-              directionsService = new google.maps.DirectionsService();
+    //  function calculateRoute(from, to) {
+    //       //use only  a single maps-instance
+    //       if (!window.mapObject) {
+    //           var myOptions = {
+    //               zoom: 10,
+    //               center: new google.maps.LatLng(40.84, 14.25),
+    //               mapTypeId: google.maps.MapTypeId.ROADMAP
+    //           };
+    //           // Draw the map
+    //           mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+    //           // you also need only a single    DirectionsService-instance
+    //           directionsService = new google.maps.DirectionsService();
 
-          }
+    //       }
 
-          var directionsRequest = {
-              origin: from,
-              destination: to,
-              travelMode: google.maps.DirectionsTravelMode.DRIVING,
-              unitSystem: google.maps.UnitSystem.METRIC
-          };
+    //       var directionsRequest = {
+    //           origin: from,
+    //           destination: to,
+    //           travelMode: google.maps.DirectionsTravelMode.DRIVING,
+    //           unitSystem: google.maps.UnitSystem.METRIC
+    //       };
 
-          directionsService.route(
-              directionsRequest,
-              function (response, status) {
-                  if (status == google.maps.DirectionsStatus.OK) {
-                      new google.maps.DirectionsRenderer({
-                          map: mapObject,
-                          directions: response
-                      });
-                  }
-                  else
-                      $("#error").append("Unable to retrieve your route<br />");
-              }
-          );
-      }
+    //       directionsService.route(
+    //           directionsRequest,
+    //           function (response, status) {
+    //               if (status == google.maps.DirectionsStatus.OK) {
+    //                   new google.maps.DirectionsRenderer({
+    //                       map: mapObject,
+    //                       directions: response
+    //                   });
+    //               }
+    //               else
+    //                   $("#error").append("Unable to retrieve your route<br />");
+    //           }
+    //       );
+    //   }
 
-      $(document).ready(function () {
-          // If the browser supports the Geolocation API
-          if (typeof navigator.geolocation == "undefined") {
-              $("#error").text("Your browser doesn't support the Geolocation API");
-              return;
-          }
+    //   $(document).ready(function () {
+    //       // If the browser supports the Geolocation API
+    //       if (typeof navigator.geolocation == "undefined") {
+    //           $("#error").text("Your browser doesn't support the Geolocation API");
+    //           return;
+    //       }
 
-          $("#from-link, #to-link").click(function (event) {
-              event.preventDefault();
-              var addressId = this.id.substring(0, this.id.indexOf("-"));
+    //       $("#from-link, #to-link").click(function (event) {
+    //           event.preventDefault();
+    //           var addressId = this.id.substring(0, this.id.indexOf("-"));
 
-              navigator.geolocation.getCurrentPosition(function (position) {
-                  var geocoder = new google.maps.Geocoder();
-                  geocoder.geocode({
-                      "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-                  },
-                      function (results, status) {
-                          if (status == google.maps.GeocoderStatus.OK)
-                              $("#" + addressId).val(results[0].formatted_address);
-                          else
-                              $("#error").append("Unable to retrieve your address<br />");
-                      });
-              },
-                  function (positionError) {
-                      $("#error").append("Error: " + positionError.message + "<br />");
-                  },
-                  {
-                      enableHighAccuracy: true,
-                      timeout: 10 * 1000 // 10 seconds
-                  });
-          });
+    //           navigator.geolocation.getCurrentPosition(function (position) {
+    //               var geocoder = new google.maps.Geocoder();
+    //               geocoder.geocode({
+    //                   "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    //               },
+    //                   function (results, status) {
+    //                       if (status == google.maps.GeocoderStatus.OK)
+    //                           $("#" + addressId).val(results[0].formatted_address);
+    //                       else
+    //                           $("#error").append("Unable to retrieve your address<br />");
+    //                   });
+    //           },
+    //               function (positionError) {
+    //                   $("#error").append("Error: " + positionError.message + "<br />");
+    //               },
+    //               {
+    //                   enableHighAccuracy: true,
+    //                   timeout: 10 * 1000 // 10 seconds
+    //               });
+    //       });
 
-          $("#calculate-route").submit(function (event) {
-              event.preventDefault();
-              calculateRoute($("#from").val(), $("#to").val());
-          });
-      });
+    //       $("#calculate-route").submit(function (event) {
+    //           event.preventDefault();
+    //           calculateRoute($("#from").val(), $("#to").val());
+    //       });
+    //   });
 
      // $("#run-search").on("click", function () {
      //      event.preventDefault();
