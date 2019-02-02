cace4ebfd31debb5a3f63277086467408c9afe14
marcobo <marcobrugo@mac.com>
2019-02-01 19:39:40 -0500

    pushing the card div body manipulation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,11 @@ $(document).ready(function () {
     var pubCounter = 0;
     var pubRemaining;
     var city;
-    var catId;
-    var limit = "25";
-    var radius = "5000";
+    var venueLat;
+    var venueLong;
+    var venueName;
     var venueId;
-    var locationLat;
-    var locationLong;
+    var venueAddress;
     var numPeople;
     var numBar;
 
@@ -21,16 +20,17 @@ $(document).ready(function () {
     $('.dropdown-trigger').dropdown();
     $('.parallax').parallax();
 
-    function updateRemainingPubs(){
-        pubRemaining = numBar-pubCounter;
+    function updateRemainingPubs() {
+        pubRemaining = numBar - pubCounter;
         $("#remaining").text(pubRemaining);
-        if (pubRemaining==0) {
+        if (pubRemaining == 0) {
             $(".add-btn").addClass("disabled");
-        }        
+            $("#submit").addClass("disabled");
+        }
     };
 
     // Start capture of input
-    $("#submit").on("click", function(event) {
+    $("#submit").on("click", function (event) {
         event.preventDefault();
 
 
@@ -39,15 +39,15 @@ $(document).ready(function () {
         // locationLong = $("#crawlersCity option:selected").attr("data-longitude");
         numPeople = $("#crawlersNum option:selected").val();
         numBar = $("#pubsNum option:selected").val();
-        
-        
-        console.log(city +"|"+ numPeople+"|"+numBar);
-        var newItinObj = {
-            city : city,
-            // locationLong : locationLong,
-            numPeople : numPeople,
-            numBar : numBar
-        };
+
+
+        console.log(city + "|" + numPeople + "|" + numBar);
+        // var newItinObj = {
+        //     city : city,
+        //     // locationLong : locationLong,
+        //     numPeople : numPeople,
+        //     numBar : numBar
+        // };
 
         // database.push(newItinObj);
 
@@ -58,86 +58,88 @@ $(document).ready(function () {
 
         var settings = {
             "async": true,
-            "url": "https://ancient-ocean-97660.herokuapp.com/categoeryId?city="+city+"&client_id=HT0AF0YCBHXL00VK1ZSEAXQVPEC3Y2UTA5RIB4UMALZZUERC&client_secret=2BBILRGSDF0ZYSAK4KH50VZSFO4FCR0MZLXPWDSZRNKQ0UST&v=20190110&radius=10&categoryId=4bf58dd8d48988d11d941735&limit=10",
+            "url": "https://ancient-ocean-97660.herokuapp.com/categoeryId?city=" + city + "&client_id=HT0AF0YCBHXL00VK1ZSEAXQVPEC3Y2UTA5RIB4UMALZZUERC&client_secret=2BBILRGSDF0ZYSAK4KH50VZSFO4FCR0MZLXPWDSZRNKQ0UST&v=20190110&radius=5&categoryId=4bf58dd8d48988d11d941735&limit=10",
             "method": "GET",
             "headers": {
                 "cache-control": "no-cache",
                 "Postman-Token": "bcf5dd2b-3c65-449d-83ba-701d5d2ce430"
             }
         }
-            
+
         $.ajax(settings).done(function (response) {
             console.log(response);
 
-        
+
             var results = response.response.venues;
-            var venueId;
+
             // console.log("results j",results[j]);
 
-            for (var j = 0; j < results.length; j++){
-                
+            for (var j = 0; j < results.length; j++) {
+
                 venueId = results[j].id;
-                var venueName = results[j].name; 
-                console.log("results j",venueName);
-                var venueAddress = results[j].location.formattedAddress;
-                
+                venueName = results[j].name;
 
-                console.log(venueId);
+                venueAddress = results[j].location.formattedAddress;
+                venueLat = results[j].location.lat;
+                venueLong = results[j].location.lng;
 
-                var settings1 = {
-                    "async": true,
-                    "url": "https://ancient-ocean-97660.herokuapp.com/venue?venueId="+venueId,
-                    "method": "GET",
-                    "headers": {
-                        "Content-Type": "application/json",
-                        "cache-control": "no-cache",
-                        "Postman-Token": "4953f006-dbbb-4411-8bf1-9a7f7781dd4d"
-                    },
-                    "processData": false,
-                    "data": ""
-                }
-                    
-                $.ajax(settings1).done(function (venue) {
-                    // console.log("VenueResponse: "+venue);
-                
-                    var venueResults = venue.response.venue;
-                    // console.log(spanVar);
-                    
 
-                // })
-                    
-                    var anchorVar = $("<a>");
-                    anchorVar.addClass("collection-item");
-                    // anchorVar.text(results[j].name);
-                    anchorVar.text(venueName);
 
-                    console.log("venue name", venueName)
 
-                    // var spanVar = $("<span>");
-                    var spanVar = $("<span>");
-                    spanVar.addClass("badge btn-flat waves-effect waves-light red add-btn");
-                    spanVar.attr("data-barname", venueName);
-                    spanVar.attr("data-baraddress", venueAddress);
-                    spanVar.attr("data-barrating", venueResults.rating);
-                    // spanVar.attr("data-barrating", "5");
+                // var settings1 = {
+                //     "async": true,
+                //     "url": "https://ancient-ocean-97660.herokuapp.com/venue?venueId="+venueId,
+                //     "method": "GET",
+                //     "headers": {
+                //         "Content-Type": "application/json",
+                //         "cache-control": "no-cache",
+                //         "Postman-Token": "4953f006-dbbb-4411-8bf1-9a7f7781dd4d"
+                //     },
+                //     "processData": false,
+                //     "data": ""
+                // }
 
-                    var iTagVar = $("<i>");
-                    iTagVar.addClass("material-icons");
-                    iTagVar.text("+");
+                // $.ajax(settings1).done(function (venue) {
+                //     // console.log("VenueResponse: "+venue);
 
-                    spanVar.prepend(iTagVar);
-                    anchorVar.prepend(spanVar);
+                //     var venueResults = venue.response.venue;
+                //     // console.log(spanVar)
 
-                    $("#pub-options").prepend(anchorVar); 
-                })
-                    
+                // // }
+                var anchorVar = $("<a>");
+                anchorVar.addClass("collection-item");
+                // anchorVar.text(results[j].name);
+                anchorVar.text(venueName);
+
+                console.log("venue name", venueName)
+
+                // var spanVar = $("<span>");
+                var spanVar = $("<span>");
+                spanVar.addClass("badge btn-flat waves-effect waves-light red add-btn");
+                spanVar.attr("data-barname", venueName);
+                spanVar.attr("data-baraddress", venueAddress);
+                // spanVar.attr("data-barrating", venueResults.rating);
+                spanVar.attr("data-barlat", venueLat);
+                spanVar.attr("data-barlong", venueLong);
+
+                var iTagVar = $("<i>");
+                iTagVar.addClass("material-icons");
+                iTagVar.text("+");
+
+                spanVar.prepend(iTagVar);
+                anchorVar.prepend(spanVar);
+
+                $("#pub-options").prepend(anchorVar);
+                // });
+
             }
             updateRemainingPubs();
 
             //modal button trigger
             var modalButVar = $("<a>");
             modalButVar.addClass("waves-effect waves-light btn modal-trigger");
-            modalButVar.attr("href","#selected-pubs");
+            modalButVar.attr("href", "#selected-pubs");
+
             modalButVar.attr("id", "#show-tour");
             modalButVar.text("Show Tour");
             $("#pub-options").append(modalButVar);
@@ -147,70 +149,139 @@ $(document).ready(function () {
 
 
     //creates the modal with the selected pub information
-    $(document).on("click", ".add-btn", function(){
+    $(document).on("click", ".add-btn", function () {
 
         $(this).addClass("disabled");
-        
-        var pubCardVar = $("<div>");
-        pubCardVar.addClass("col s6");
-        pubCardVar.attr("id","pub-card");
 
-        var cardVar = $("<div>");
-        cardVar.addClass("card blue-grey darken-1");
+        var pubCardDiv = $("<div>");
+        pubCardDiv.addClass("col-6");
+        pubCardDiv.attr("id", "pub-card-div");
+        pubCardDiv.attr("style", "height:auto;");
 
-        var cardContentVar = $("<div>");
-        cardContentVar.addClass("card-content white-text");
+        var pubCard = $("<div>");
+        pubCard.addClass("card blue-grey darken-1");
 
-        var cardTitleVar = $("<span>");
-        cardTitleVar.addClass("card-title");
-        cardTitleVar.attr("id","bar-name");
-        cardTitleVar.text($(this).attr("data-barname"));
+        var cardContent = $("<div>");
+        cardContent.addClass("card-content white-text");
+        cardContent.attr("style", "height:auto;");
 
-        var pAddressVar = $("<p>");
-        pAddressVar.text("Address: "+$(this).attr("data-baraddress"));
 
-        var pRatingVar = $("<p>");
-        pRatingVar.text("Rating: "+$(this).attr("data-barrating"));
+        var cardTitle = $("<span>");
+        cardTitle.addClass("card-title");
+        cardTitle.attr("id", "bar-name");
 
-        cardContentVar.append(cardTitleVar);
-        cardContentVar.append(pAddressVar);
-        cardContentVar.append(pRatingVar);
-        
-        cardVar.append(cardContentVar);
+        cardTitle.text($(this).attr("data-barname"));
 
-        pubCardVar.append(cardVar);
+        var pubAddress = $("<p>");
+        pubAddress.text("Address: " + $(this).attr("data-baraddress"));
 
-        $("#pubs-container").append(pubCardVar);
+
+        cardContent.append(cardTitle);
+        cardContent.append(pubAddress);
+
+        pubCard.append(cardContent);
+
+        pubCardDiv.append(pubCard);
+
+        $("#pubs-container").append(pubCardDiv);
         pubCounter++;
         updateRemainingPubs();
 
+
+
     });
-    // var mapProp= {
-    //     center:new google.maps.LatLng(51.508742,-0.120850),
-    //     zoom:5,
-    //   };
-    //   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    //   }
-    //   });
-    // var querylink= "https://maps.googleapis.com/maps/api/js?key=AIzaSyBMD3ojp-P28ggup4xeFUZI9i6rYDJwRNU";
-    // console.log(querylink);
-    // $.ajax()({
-    //     url=querylink
-    //     method="GET"
-    //    });
-    // function myMap() {
-    //     var mapProp= {
-    //       center:new google.maps.LatLng(51.508742,-0.120850),
-    //       zoom:5,
-    //     };
-    //     var map = new google.maps.Map(document.getElementById("map"),mapProp);
-    //     }
-    
+    $(document).on("click", "#make-reservation", function () {
 
-      
+        var userBarName = $("#pub-crawl-name").val();
+        console.log(userBarName);
 
+        //creating div row
+        var cardRow = $("<div>");
+        cardRow.attr("class", "row");
+
+
+        //creating div columns
+        var cardDivThree = $("<div>");
+        cardDivThree.attr("class", "col-lg-3");
+        cardDivThree.attr("style", "margin-top: 30px;");
+
+
+        // cardRow.append(cardDivThree);
+        //creating a new card
+        var newCardDiv = $("<div>");
+        newCardDiv.attr("class", "card");
+        newCardDiv.attr("id", "viewexisting");
+
+        cardDivThree.append(newCardDiv)
+
+        //creating the card type
+        cardImgDiv = $("<div>");
+        cardImgDiv.attr("class", "card-image waves-effect waves-block waves-light");
+
+        //creating and appending card image
+        var pictureNum = Math.floor(Math.random() * 5) + 1;
+        console.log(pictureNum);
+        cardImg = $("<img>");
+        cardImg.attr("src", "assets/images/cardPicMiami.png");
+        cardImg.attr("alt", "randomImg");
+        cardImg.attr("class", "activator");
+
+        //appending image to card
+        cardImgDiv.append(cardImg);
+        newCardDiv.append(cardImgDiv);
+
+        //Creating and appending card content in div
+        var cardContentDiv = $("<div>");
+        cardContentDiv.attr("class", "card-conent");
+
+
+        newCardDiv.append(cardContentDiv);
+
+        //Need to add the value of the content after user inputs go to firebase.
+
+        var spanCardTitle = $("<span>");
+        spanCardTitle.attr("class", "card-title activator grey-text text-darken-4");
+        //need to append card title from user input
+        spanCardTitle.append(userBarName);
+        newCardDiv.append(spanCardTitle);
+
+        //creating and appending i Div
+        var iDiv = $("<i>");
+
+        iDiv.attr("class", "material-icons righqt");
+        iDiv.attr("style", "font-size:35px;");
+
+        //adding city name from user input. 
+        var cityName = city;
+        var pCityName = $("#city-name");
+        pCityName.append(cityName);
+        newCardDiv.append(iDiv);
+
+
+        //Adding card reveal
+        var cardReveal = $("<div>");
+        cardReveal.attr("class", "card-reveal");
+        cardReveal.attr("style", "font-size:35px;");
+        var spanRevealCardTitle = $("<span>");
+        spanRevealCardTitle.attr("class", "card-title grey-text text-darken-4");
+        //need to append card title from user input
+        spanRevealCardTitle.append(userBarName);
+        newCardDiv.append(spanCardTitle);
+
+        var iDivReveal = $("<i>");
+        iDivReveal.attr("class", "material-icons righqt");
+        iDivReveal.attr("style", "font-size:35px;");
+        iDivReveal.text("X");
+
+        newCardDiv.append(iDivReveal);
+        
+        $("#viewexisting").append(newCardDiv);
+
+    });
 
 });
+
+
 
 
 // pub category Id: 52e81612bcbc57f1066b7a06, 4bf58dd8d48988d11b941735
@@ -221,5 +292,7 @@ $(document).ready(function () {
 
 //query for venues: "https://api.foursquare.com/v2/venues/search?client_id=HT0AF0YCBHXL00VK1ZSEAXQVPEC3Y2UTA5RIB4UMALZZUERC&client_secret=2BBILRGSDF0ZYSAK4KH50VZSFO4FCR0MZLXPWDSZRNKQ0UST&near=" + city + "&categoryId=" + catId + "limit=" + limit + "&radius=" + radius;
 //query for rating: "https://api.foursquare.com/v2/venues/" + venueId + "?client_id=HT0AF0YCBHXL00VK1ZSEAXQVPEC3Y2UTA5RIB4UMALZZUERC&client_secret=2BBILRGSDF0ZYSAK4KH50VZSFO4FCR0MZLXPWDSZRNKQ0UST";
+
+
 
 
